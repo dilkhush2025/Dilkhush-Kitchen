afb55ca8fe35a49542ae88495f8518f7481f313f
dilkhush2025 <235.dilkhush@gmail.com>
2026-01-25 00:16:27 +0000

    delete popup image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ImageGallery from '../../components/ImageGallery/ImageGallery';
 import Header from '../../components/Header/Header';
 import AboutUsSection from '../../components/AboutUsSection/AboutUsSection';
@@ -7,37 +7,9 @@ import './Home.css';  // make sure to import the CSS
 
 function Home() {
 
-  // 🔹 ADD: popup state
-  const [showPopup, setShowPopup] = useState(false);
-
-  // 🔹 ADD: show popup on page load
-  useEffect(() => {
-    setShowPopup(true);
-  }, []);
-
   return (
     <div className='home-page'>
 
-        {/* 🔹 ADD: Popup Image */}
-        {showPopup && (
-          <div className="popup-overlay">
-            <div className="popup-content">
-              <button
-                className="popup-close"
-                onClick={() => setShowPopup(false)}
-              >
-                &times;
-              </button>
-
-              <img
-                src={require('../../utils/images/popup-image.jpg')}
-                alt="Popup"
-              />
-            </div>
-          </div>
-        )}
-
-        {/* ✅ EXISTING CODE (unchanged) */}
         <Header customClass="home-header" />
 
         <AboutUsSection />
