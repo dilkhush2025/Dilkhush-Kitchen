8b08df2a580f1901e36c2f2f65ac28d9dde2ddec
dilkhush2025 <235.dilkhush@gmail.com>
2026-01-30 23:39:27 +0000

    pettis pop up error useeffect issue

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import ImageGallery from '../../components/ImageGallery/ImageGallery';
 import Header from '../../components/Header/Header';
 import AboutUsSection from '../../components/AboutUsSection/AboutUsSection';
 import ContactSection from '../../components/ContactSection/ContactSection';
+import React, { useState, useEffect } from 'react';
 import './Home.css';  // make sure to import the CSS
 
 function Home() {
