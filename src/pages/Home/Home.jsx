62d3cb87630ebfeddd00a470885818ac232130f9
dilkhush2025 <235.dilkhush@gmail.com>
2026-01-30 23:35:41 +0000

    added pettis popup image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,8 +7,36 @@ import './Home.css';  // make sure to import the CSS
 
 function Home() {
 
+  // 🔹 ADD: popup state
+  const [showPopup, setShowPopup] = useState(false);
+
+  // 🔹 ADD: show popup on page load
+  useEffect(() => {
+    setShowPopup(true);
+  }, []);
+
   return (
     <div className='home-page'>
+      {/* 🔹 ADD: Popup Image */}
+        {showPopup && (
+          <div className="popup-overlay">
+            <div className="popup-content">
+              <button
+                className="popup-close"
+                onClick={() => setShowPopup(false)}
+              >
+                &times;
+              </button>
+
+              <img
+                src={require('../../utils/images/popup-image.jpg')}
+                alt="Popup"
+              />
+            </div>
+          </div>
+        )}
+
+        {/* ✅ EXISTING CODE (unchanged) */}
 
         <Header customClass="home-header" />
 
