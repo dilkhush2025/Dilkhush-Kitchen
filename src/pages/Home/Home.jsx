09c7d334bcfcdef2f675efc6ce44ead4d63abffc
dilkhush2025 <235.dilkhush@gmail.com>
2026-04-04 12:31:26 +0100

    remove pop up

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,13 @@ import ImageGallery from '../../components/ImageGallery/ImageGallery';
 import Header from '../../components/Header/Header';
 import AboutUsSection from '../../components/AboutUsSection/AboutUsSection';
 import ContactSection from '../../components/ContactSection/ContactSection';
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import './Home.css';  // make sure to import the CSS
 
 function Home() {
 
   // 🔹 ADD: popup state
-  const [showPopup, setShowPopup] = useState(false);
+  // const [showPopup, setShowPopup] = useState(false);
 
   // 🔹 ADD: show popup on page load
   // useEffect(() => {
